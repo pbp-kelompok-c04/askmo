fcccbc07f46621cbbfa7c5434eb94a686ef3d8ce
Lessyarta Kamali Sopamena Pirade <190507046+kamali-pirade@users.noreply.github.com>
2025-10-10 08:53:10 +0700

    Pengaturan awal autentikasi user (login, register, logout) dan membuat UI dasar (main page, navigation bar, notifikasi toast)

--- main/models.py
+++ main/models.py
@@ -1,3 +1,82 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Lapangan(models.Model):
+    OLAHRAGA_CHOICES = [
+        ('sepakbola', 'Sepak Bola'),
+        ('basket', 'Basket'),
+        ('voli', 'Voli'),
+        ('badminton', 'Badminton'),
+        ('tenis', 'Tenis'),
+        ('futsal', 'Futsal'),
+        ('padel', 'Padel'),
+        ('golf', 'Golf'),
+        ('lainnya', 'Lainnya'),
+    ]
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nama = models.CharField(max_length=255)
+    deskripsi = models.TextField()
+    olahraga = models.CharField(max_length=20, choices=OLAHRAGA_CHOICES, default='update')
+    thumbnail = models.URLField(blank=True, null=True)
+    rating = models.FloatField(default=0.0)
+    refund = models.BooleanField(default=False)
+    tarif_per_sesi = models.DecimalField(max_digits=10, decimal_places=2)
+    kontak = models.CharField(max_length=100)
+    alamat = models.TextField()
+    review = models.TextField(blank=True, null=True)
+    peraturan = models.TextField(blank=True, null=True)
+    fasilitas = models.TextField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nama
+    
+class Coach(models.Model):
+    OLAHRAGA_CHOICES = [
+        ('sepakbola', 'Sepak Bola'),
+        ('basket', 'Basket'),
+        ('voli', 'Voli'),
+        ('badminton', 'Badminton'),
+        ('tenis', 'Tenis'),
+        ('futsal', 'Futsal'),
+        ('padel', 'Padel'),
+        ('golf', 'Golf'),
+        ('lainnya', 'Lainnya'),
+    ]
+        
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nama = models.CharField(max_length=255)
+    olahraga = models.CharField(max_length=20, choices=OLAHRAGA_CHOICES, default='update')
+    deskripsi = models.TextField()
+    kontak = models.CharField(max_length=100)
+    tarif_per_jam = models.DecimalField(max_digits=10, decimal_places=2)
+    thumbnail = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nama
+    
+class Event(models.Model):
+    OLAHRAGA_CHOICES = [
+        ('sepakbola', 'Sepak Bola'),
+        ('basket', 'Basket'),
+        ('voli', 'Voli'),
+        ('badminton', 'Badminton'),
+        ('tenis', 'Tenis'),
+        ('futsal', 'Futsal'),
+        ('padel', 'Padel'),
+        ('golf', 'Golf'),
+        ('lainnya', 'Lainnya'),
+    ]
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nama = models.CharField(max_length=255)
+    olahraga = models.CharField(max_length=20, choices=OLAHRAGA_CHOICES, default='update')
+    deskripsi = models.TextField()
+    tanggal = models.DateField()
+    lokasi = models.CharField(max_length=255)
+    kontak = models.CharField(max_length=100)
+    biaya = models.DecimalField(max_digits=10, decimal_places=2)
+    thumbnail = models.URLField(blank=True, null=True)
+    jam = models.TimeField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nama
