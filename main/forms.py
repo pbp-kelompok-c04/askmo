a282b3a775777190f2c7af9fae03cef850229a3e
alyaanaazz <nisrinaalyanabilah@gmail.com>
2025-10-21 19:00:59 +0700

    merging

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from main.models import Lapangan, Coach, Event
 
 
@@ -6,6 +7,14 @@ class LapanganForm(ModelForm):
     class Meta:
         model = Lapangan
         fields = ["nama", "deskripsi", "olahraga", "thumbnail", "rating", "refund", "tarif_per_sesi", "kontak", "alamat", "review", "peraturan", "fasilitas"]
+
+    def clean_rating(self):
+        rating = self.cleaned_data.get("rating",0.0)
+        if not (0.0 <= rating <= 5.0):
+            raise forms.ValidationError("Rating harus antara 0.0 sampai 5.0")
+        return rating
+        
+
 
 class CoachForm(ModelForm):
     class Meta:
