8f8d03256f4e88c616bade1ce2193f8f5864ad4c
matt-wijaya <matthew.wijaya@ui.ac.id>
2025-10-24 22:55:01 +0700

    Fix CSRF Token

--- askmo/settings.py
+++ askmo/settings.py
@@ -32,7 +32,9 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "lessyarta-kamali-askmo.pbp.cs.ui.ac.id"]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://lessyarta-kamali-askmo.pbp.cs.ui.ac.id/"
+]
 # Application definition
 
 INSTALLED_APPS = [
