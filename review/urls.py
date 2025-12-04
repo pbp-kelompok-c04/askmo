dc75569174e7b364b805f1e164d5f8be6cd22536
syafiq-f <syafiq.fqh@gmail.com>
2025-12-04 16:03:54 +0700

    sedikti change untuk ambil review json and penyesuaian di html

--- review/urls.py
+++ review/urls.py
@@ -17,14 +17,31 @@ urlpatterns = [
     path('lapangan/json-single/<int:review_id>/', views.get_single_review_json, name='get_single_review_json'),
     path('lapangan/edit/<int:review_id>/', views.show_edit_review_lapangan, name='show_edit_review_lapangan'),
     path('lapangan/<int:lapangan_id>/reviews/', views.get_reviews_json_lapangan, name='reviews-lapangan'),
+ 
+    # =================== REVIEW COACH ===================
+    path('review-coach/<int:coach_id>/',
+         views.show_feeds_review_coach, name='show_feeds_review_coach'),
 
-    
-    # REVIEW COACH
-    path('coach/<int:coach_id>/', views.show_feeds_review_coach, name='show_feeds_review_coach'),
-    path('coach/<int:coach_id>/add/', views.show_form_review_coach, name='show_form_review_coach'),
-    path('coach/edit/<int:review_id>/', views.edit_review_coach, name='edit_review_coach'),
-    path('coach/delete/<int:review_id>/', views.delete_review_coach, name='delete_review_coach'),
-    path('coach/json/<int:coach_id>/', views.get_reviews_json, name='get_reviews_json_coach'), 
+    path('review-coach/<int:coach_id>/add/',
+         views.show_form_review_coach, name='show_form_review_coach'),
+
+    path('review-coach/edit/<int:review_id>/',
+         views.edit_review_coach, name='edit_review_coach'),
+
+    path('review-coach/delete/<int:review_id>/',
+         views.delete_review_coach, name='delete_review_coach'),
+
+    path('review-coach/json/<int:coach_id>/',
+         views.get_reviews_json, name='get_reviews_json_coach'),
+
+    path('review-coach/add-ajax/<int:coach_id>/',
+         views.add_review_coach_ajax, name='add_review_coach_ajax'),
+
+    path('review-coach/edit-ajax/<int:review_id>/',
+         views.edit_review_coach_ajax, name='edit_review_coach_ajax'),
+
+    path('coach/add-ajax/<int:coach_id>/', views.add_review_coach_ajax, name='add_review_coach_ajax'),
 
     path('auth/', include('authentication.urls')),
+    path('coach/edit-ajax/<int:review_id>/', views.edit_review_coach_ajax, name='edit_review_coach_ajax'),
 ]
